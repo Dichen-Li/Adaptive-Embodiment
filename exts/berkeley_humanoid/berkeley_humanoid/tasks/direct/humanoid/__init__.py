6a679da2d8625d290f61a12083eedb70a214c902
Bo Ai <albert_0110@outlook.com>
2024-10-31 23:43:52 -0700

    add berkeley env

--- exts/berkeley_humanoid/berkeley_humanoid/tasks/direct/humanoid/__init__.py
+++ exts/berkeley_humanoid/berkeley_humanoid/tasks/direct/humanoid/__init__.py
@@ -13,7 +13,7 @@ from . import agents
 from .humanoid_env import HumanoidEnv, HumanoidEnvCfg
 from .h1_direct_env import H1DirectEnv, H1EnvCfg
 from .g1_direct_env import G1DirectEnv, G1EnvCfg
-
+from .berkeley_humanoid_direct_env import BerkeleyHumanoidDirectEnv, BerkeleyHumanoidEnvCfg
 
 ##
 # Register Gym environments.
@@ -38,6 +38,18 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": H1EnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Berkeley-Direct-v0",
+    entry_point="berkeley_humanoid.tasks.direct.humanoid:BerkeleyHumanoidDirectEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": BerkeleyHumanoidEnvCfg,
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
