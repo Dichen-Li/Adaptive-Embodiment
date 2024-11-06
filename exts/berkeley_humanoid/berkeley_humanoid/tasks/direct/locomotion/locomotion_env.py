c67ffcbc90f3d898a27cf10d9d869480181c5485
Bo Ai <albert_0110@outlook.com>
2024-11-05 22:23:35 -0800

    adjust loss weight

--- exts/berkeley_humanoid/berkeley_humanoid/tasks/direct/locomotion/locomotion_env.py
+++ exts/berkeley_humanoid/berkeley_humanoid/tasks/direct/locomotion/locomotion_env.py
@@ -331,7 +331,7 @@ class LocomotionEnv(DirectRLEnv):
             'feet_air_time': feet_air_time * 2.0,
             'feet_slide': feet_slide * -0.25,
             'undesired_contacts': undesired_contacts * -0.1,
-            'joint_deviation_hip': joint_deviation_hip * -0.1,
+            'joint_deviation_hip': joint_deviation_hip * -0.1 * 5,
             'joint_deviation_knee': joint_deviation_knee * -0.01
         }
         total_reward = sum(self.reward_dict.values())
