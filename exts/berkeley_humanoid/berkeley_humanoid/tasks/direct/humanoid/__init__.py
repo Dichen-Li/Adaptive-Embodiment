5df516a99983555386efcd0e1c4fb86f6857af07
Bo Ai <albert_0110@outlook.com>
2024-11-05 22:23:22 -0800

    add go2 training

--- exts/berkeley_humanoid/berkeley_humanoid/tasks/direct/humanoid/__init__.py
+++ exts/berkeley_humanoid/berkeley_humanoid/tasks/direct/humanoid/__init__.py
@@ -13,11 +13,24 @@ from . import agents
 from .humanoid_env import HumanoidEnv, HumanoidEnvCfg
 from .h1_direct_env import H1DirectEnv, H1EnvCfg
 from .g1_direct_env import G1DirectEnv, G1EnvCfg
+from .go2_direct_env import Go2DirectEnv, Go2EnvCfg
 from .berkeley_humanoid_direct_env import BerkeleyHumanoidDirectEnv, BerkeleyHumanoidEnvCfg
 
 ##
 # Register Gym environments.
 ##
+
+gym.register(
+    id="Go2-Direct-v0",
+    entry_point="berkeley_humanoid.tasks.direct.humanoid:Go2DirectEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": Go2EnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
 
 gym.register(
     id="G1-Direct-v0",
