3b5f5bb742b46afd8adb9fdbbb3c1553f2547eaf
Bo Ai <albert_0110@outlook.com>
2024-11-07 10:18:46 -0800

    update go2 usd path

--- exts/berkeley_humanoid/berkeley_humanoid/assets/unitree.py
+++ exts/berkeley_humanoid/berkeley_humanoid/assets/unitree.py
@@ -138,8 +138,8 @@ from berkeley_humanoid.assets import ISAAC_ASSET_DIR
 
 GO2_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
-        # usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/Unitree/Go2/go2.usd",
-        usd_path=f"{ISAAC_ASSET_DIR}/Robots/Unitree/Go2/go2.usd",
+        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/Unitree/Go2/go2.usd",
+        # usd_path=f"{ISAAC_ASSET_DIR}/Robots/Unitree/Go2/go2.usd",   # we can't use it because the Prop folder that contains instantiable mesh is missing; warning thrown
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
