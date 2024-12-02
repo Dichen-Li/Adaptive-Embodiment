833651f7e7f0599f7968964de1ffc0f37889e5ed
Dichen Li <lchanggle2022@gmail.com>
2024-12-02 12:19:16 -0800

    in modifying the train_supervised_one_policy.py

--- scripts/rsl_rl/train_supervised_one_policy.py
+++ scripts/rsl_rl/train_supervised_one_policy.py
@@ -14,30 +14,30 @@ from omni.isaac.lab.app import AppLauncher
 # local imports
 import cli_args  # isort: skip
 
-# add argparse arguments
-parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
-parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
-parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
-parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
-parser.add_argument(
-    "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
-)
-parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
-parser.add_argument("--task", type=str, default=None, help="Name of the task.")
-parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
-parser.add_argument("--max_iterations", type=int, default=None, help="RL Policy training iterations.")
-# append RSL-RL cli arguments
-cli_args.add_rsl_rl_args(parser)
-# append AppLauncher cli args
-AppLauncher.add_app_launcher_args(parser)
-args_cli = parser.parse_args()
-# always enable cameras to record video
-if args_cli.video:
-    args_cli.enable_cameras = True
+# # add argparse arguments
+# parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
+# parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
+# parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
+# parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
+# parser.add_argument(
+#     "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
+# )
+# parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
+# parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+# parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
+# parser.add_argument("--max_iterations", type=int, default=None, help="RL Policy training iterations.")
+# # append RSL-RL cli arguments
+# cli_args.add_rsl_rl_args(parser)
+# # append AppLauncher cli args
+# AppLauncher.add_app_launcher_args(parser)
+# args_cli = parser.parse_args()
+# # always enable cameras to record video
+# if args_cli.video:
+#     args_cli.enable_cameras = True
 
-# launch omniverse app
-app_launcher = AppLauncher(args_cli)
-simulation_app = app_launcher.app
+# # launch omniverse app
+# app_launcher = AppLauncher(args_cli)
+# simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
@@ -46,78 +46,63 @@ import os
 import torch
 from datetime import datetime
 
-from rsl_rl.runners import OnPolicyRunner
-
 # Import extensions to set up environment tasks
-import berkeley_humanoid.tasks  # noqa: F401
+# import berkeley_humanoid.tasks  # noqa: F401
 
-from omni.isaac.lab.envs import ManagerBasedRLEnvCfg
-from omni.isaac.lab.utils.dict import print_dict
-from omni.isaac.lab.utils.io import dump_pickle, dump_yaml
-from omni.isaac.lab_tasks.utils import get_checkpoint_path, parse_env_cfg
-from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlVecEnvWrapper
 
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 torch.backends.cudnn.deterministic = False
 torch.backends.cudnn.benchmark = False
 
+import numpy as np
+from berkeley_humanoid.tasks.direct.locomotion.locomotion_env import LocomotionEnv
+
+env = LocomotionEnv()
+state = env
+# Create policy and critic state masks
+policy_general_state_mask = np.zeros(state.shape)
+critic_general_state_mask = np.zeros(state.shape)
+general_state_for_policy_names = [
+    "trunk_roll_velocity", "trunk_pitch_velocity", "trunk_yaw_velocity",
+    "goal_x_velocity", "goal_y_velocity", "goal_yaw_velocity",
+    "projected_gravity_x", "projected_gravity_y", "projected_gravity_z",
+    "p_gain", "d_gain", "action_scaling_factor",
+    "mass",
+    "robot_length", "robot_width", "robot_height"
+]
+general_state_for_critic_names = [
+    "trunk_x_velocity", "trunk_y_velocity", "trunk_z_velocity",
+    "trunk_roll_velocity", "trunk_pitch_velocity", "trunk_yaw_velocity",
+    "goal_x_velocity", "goal_y_velocity", "goal_yaw_velocity",
+    "projected_gravity_x", "projected_gravity_y", "projected_gravity_z",
+    "height_0",
+    "p_gain", "d_gain", "action_scaling_factor",
+    "mass",
+    "robot_length", "robot_width", "robot_height"
+]
+observation_name_to_ids = self.env.call("observation_name_to_id")
+for env_id in range(self.nr_train_envs):
+    for name in general_state_for_policy_names:
+        self.policy_general_state_mask[env_id, observation_name_to_ids[env_id][name]] = 1
+
+    for name in general_state_for_critic_names:
+        self.critic_general_state_mask[env_id, observation_name_to_ids[env_id][name]] = 1
+self.policy_general_state_mask = np.array(self.policy_general_state_mask, dtype=bool)
+self.critic_general_state_mask = np.array(self.critic_general_state_mask, dtype=bool)
+
+dummy_dynamic_joint_combined_state = state[:self.train_env_ids[1], :self.dynamic_joint_observation_lengths[self.train_env_ids[0]]].reshape((-1, self.nr_dynamic_joint_observations[self.train_env_ids[0]], self.single_dynamic_joint_observation_length))
+dummy_dynamic_joint_description = dummy_dynamic_joint_combined_state[:, :, :self.dynamic_joint_description_size]
+dummy_dynamic_joint_state = dummy_dynamic_joint_combined_state[:, :, self.dynamic_joint_description_size:]
+
+dummy_dynamic_foot_combined_state = state[:self.train_env_ids[1], self.dynamic_joint_observation_lengths[self.train_env_ids[0]]:self.dynamic_joint_observation_lengths[self.train_env_ids[0]] + self.dynamic_foot_observation_lengths[self.train_env_ids[0]]].reshape((-1, self.nr_dynamic_foot_observations[self.train_env_ids[0]], self.single_dynamic_foot_observation_length))
+dummy_dynamic_foot_description = dummy_dynamic_foot_combined_state[:, :, :self.dynamic_foot_description_size]
+dummy_dynamic_foot_state = dummy_dynamic_foot_combined_state[:, :, self.dynamic_foot_description_size:]
+
+dummy_general_policy_state = state[:self.train_env_ids[1], self.policy_general_state_mask[self.train_env_ids[0]]]
+dummy_general_critic_state = state[:self.train_env_ids[1], self.critic_general_state_mask[self.train_env_ids[0]]]
 
 def main():
-    """Train with RSL-RL agent."""
-    # parse configuration
-    env_cfg: ManagerBasedRLEnvCfg = parse_env_cfg(
-        args_cli.task, num_envs=args_cli.num_envs, use_fabric=not args_cli.disable_fabric
-    )
-    agent_cfg: RslRlOnPolicyRunnerCfg = cli_args.parse_rsl_rl_cfg(args_cli.task, args_cli)
-
-    # specify directory for logging experiments
-    log_root_path = os.path.join("logs", "rsl_rl", agent_cfg.experiment_name)
-    log_root_path = os.path.abspath(log_root_path)
-    print(f"[INFO] Loading experiment from directory: {log_root_path}")
-    resume_path = get_checkpoint_path(log_root_path, agent_cfg.load_run, agent_cfg.load_checkpoint)
-    log_dir = os.path.dirname(resume_path)
-
-    # max iterations for training
-    if args_cli.max_iterations:
-        agent_cfg.max_iterations = args_cli.max_iterations
-
-    # create isaac environment
-    env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
-    # wrap for video recording
-    if args_cli.video:
-        video_kwargs = {
-            "video_folder": os.path.join(log_dir, "videos", "train"),
-            "step_trigger": lambda step: step % args_cli.video_interval == 0,
-            "video_length": args_cli.video_length,
-            "disable_logger": True,
-        }
-        print("[INFO] Recording videos during training.")
-        print_dict(video_kwargs, nesting=4)
-        env = gym.wrappers.RecordVideo(env, **video_kwargs)
-    # wrap around environment for rsl-rl
-    env = RslRlVecEnvWrapper(env)
-
-    # create runner from rsl-rl
-    runner = OnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device)
-    # write git state to logs
-    runner.add_git_repo_to_log(__file__)
-    # save resume path before creating a new log_dir
-    if agent_cfg.resume:
-        # get path to previous checkpoint
-        resume_path = get_checkpoint_path(log_root_path, agent_cfg.load_run, agent_cfg.load_checkpoint)
-        print(f"[INFO]: Loading model checkpoint from: {resume_path}")
-        # load previously trained model
-        runner.load(resume_path)
-
-    # set seed of the environment
-    env.seed(agent_cfg.seed)
-
-    # dump the configuration into log-directory
-    dump_yaml(os.path.join(log_dir, "params", "env.yaml"), env_cfg)
-    dump_yaml(os.path.join(log_dir, "params", "agent.yaml"), agent_cfg)
-    dump_pickle(os.path.join(log_dir, "params", "env.pkl"), env_cfg)
-    dump_pickle(os.path.join(log_dir, "params", "agent.pkl"), agent_cfg)
 
     # define the device = 'cuda:0'
     model_device = agent_cfg.device
@@ -132,7 +117,7 @@ def main():
         return
     # Load h5py input output data
     data = h5py.File(h5py_record_file_path, "a")
-    inputs = data["observations"]
+    inputs = data["one_policy_observation"]
     targets = data["actions"]
     # Convert to PyTorch tensors
     inputs = torch.tensor(np.array(inputs), dtype=torch.float32).to(model_device)
@@ -146,11 +131,6 @@ def main():
     # Define loss function and optimizer
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(runner.alg.actor_critic.parameters(), lr=0.001)
-    runner.train_mode()
-    # runner.learn(num_learning_iterations=agent_cfg.max_iterations, init_at_random_ep_len=True)
-
-    from training.algorithms.uni_ppo.ppo.general_properties import GeneralProperties
-    from training.algorithms.uni_ppo.ppo.policy import get_policy
 
     # Store epoch losses in a list during training
     epoch_losses = []
