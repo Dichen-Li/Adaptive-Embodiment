3db014f2e36df21c6876704092f6202cb8a1e299
Bo Ai <albert_0110@outlook.com>
2024-12-13 14:57:28 -0800

    remove unused import

--- exts/berkeley_humanoid/berkeley_humanoid/tasks/environments/gen_direct_env.py
+++ exts/berkeley_humanoid/berkeley_humanoid/tasks/environments/gen_direct_env.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from berkeley_humanoid.tasks.configs.environment.gen_quadrupeds_cfg import GenDogEnvCfg
 from berkeley_humanoid.tasks.environments.locomotion_env import LocomotionEnv
 from berkeley_humanoid.tasks.environments.locomotion_env_multi_embodi import LocomotionEnvMultiEmbodiment
 
