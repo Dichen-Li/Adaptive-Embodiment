a4d52b34ac6575a0891c194c63605152c10e75b1
TongzhouMu <tongzhou.mu@gamil.com>
2025-01-05 05:40:07 -0800

    small fix

--- scripts/generate_videos_batch_tmu.sh
+++ scripts/generate_videos_batch_tmu.sh
@@ -1,5 +1,5 @@
 for i in {0..330}
 do
-  python scripts/rsl_rl/play.py --video --headless --num_envs 2 --task Genhumanoid$i
+  /workspace/isaaclab/isaaclab.sh -p scripts/rsl_rl/play.py --video --headless --num_envs 2 --task Genhumanoid$i
   echo "===============================$i done==============================="
 done
