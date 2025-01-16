13e3ec1adcc49a6bdbb03ea3d297cbf642b02802
Dichen Li <lchanggle2022@gmail.com>
2025-01-15 16:33:06 -0800

    preparation for data collection

--- generation/gen_nautilus_jobs_quadruped_play_collect_data.py
+++ generation/gen_nautilus_jobs_quadruped_play_collect_data.py
@@ -1,15 +1,17 @@
 import os
+import time
 
 # Configuration
-num_tasks = 308  # Total number of tasks
+task_indices = list(range(168))
 tasks_prefix = "Gendog"
 tasks_suffix = ""  # Add any suffix if needed
 tasks_per_job = 21  # Number of tasks per job
 num_parallel_commands = 4  # Number of parallel commands per job
-job_name_template = "dichen-job-quadruped-{job_index}-play-collect-data-jan4-2"
+job_name_template = "dichen-job-quadruped-{job_index}-play-collect-data-jan15"
 output_folder = "jobs_play_collect_data"  # Folder to store YAML files
 submission_script = "submit_jobs_play_collect_data.sh"  # Batch submission script
 deletion_script = "delete_jobs_play_collect_data.sh"  # Batch deletion script
+sleep_interval = 50  # Time interval (in seconds) between parallel commands to prevent errors
 
 # Ensure the output folder exists
 os.makedirs(output_folder, exist_ok=True)
@@ -48,6 +50,8 @@ spec:
           volumeMounts:
             - name: dshm
               mountPath: /dev/shm
+            - name: dichen-fast-vol
+              mountPath: /dichen-fast-vol
             - name: bai-fast-vol  # change this based on your own pvc
               mountPath: /bai-fast-vol # change this based on your own pvc
             - name: liudai-fast-vol  # change this based on your own pvc
@@ -58,6 +62,9 @@ spec:
         - name: dshm # shared memory, required for the multi-worker dataloader
           emptyDir:
             medium: Memory
+        - name: dichen-fast-vol
+          persistentVolumeClaim:
+            claimName: dichen-fast-vol
         - name: bai-fast-vol  # change this based on your own pvc
           persistentVolumeClaim:
             claimName: bai-fast-vol  # change this based on your own pvc; you can get the pvc name of your created volume from "kubectl get pvc"
@@ -78,6 +85,10 @@ spec:
                     values:
                       - NVIDIA-GeForce-RTX-4090
                       - NVIDIA-GeForce-RTX-3090
+                      - NVIDIA-A100-80GB-PCIe-MIG-1g.10gb
+                      - NVIDIA-A100-PCIe-40GB
+                      - NVIDIA-A100-PCIe-80GB
+                      - NVIDIA-A100-SXM4-80GB
                       - NVIDIA-RTX-A6000
                       # - NVIDIA-GeForce-RTX-2080-Ti
                       - NVIDIA-A10
@@ -87,20 +98,27 @@ spec:
 # Create YAML files and collect job names
 job_files = []
 job_names = []
-for i in range(0, num_tasks, tasks_per_job):
+
+for i in range(0, len(task_indices), tasks_per_job):
     # Collect tasks for the current job
-    tasks = [f"{tasks_prefix}{j}{tasks_suffix}" for j in range(i, min(i + tasks_per_job, num_tasks))]
+    tasks = [f"{tasks_prefix}{task_indices[j]}{tasks_suffix}" for j in range(i, min(i + tasks_per_job, len(task_indices)))]
 
     # Split tasks into parallel groups
     task_groups = [tasks[j::num_parallel_commands] for j in range(num_parallel_commands)]
     parallel_commands = " &\n              ".join(
+        f"(sleep {i * sleep_interval} && "  # Delay the start of each task group by `i * sleep_interval` seconds
         f"source ~/.bashrc && cd /bai-fast-vol/code/embodiment-scaling-law && "
+        f"/workspace/isaaclab/_isaac_sim/python.sh -m pip install --upgrade pip && "
+        f"/workspace/isaaclab/_isaac_sim/python.sh -m pip install setuptools wheel && "
+        f"/workspace/isaaclab/_isaac_sim/python.sh -m pip install build && "
+        f"/workspace/isaaclab/_isaac_sim/python.sh -m pip install toml && "   # I know its too much but we need these to avoid werid errors.. 
         f"/workspace/isaaclab/_isaac_sim/python.sh -m pip install -e exts/berkeley_humanoid && "
-        f"/workspace/isaaclab/_isaac_sim/python.sh -m pip install -e rsl_rl/ && "   # only for sim2real_learning branch
-        f"bash scripts/play_collect_data_batch_nautilus.sh --tasks {' '.join(group)}"
-        for group in task_groups if group
+        f"/workspace/isaaclab/_isaac_sim/python.sh -m pip install -e rsl_rl && "  # only for sim2real_learning branch
+        f"bash scripts/play_collect_data_batch_nautilus.sh --tasks {' '.join(group)})"
+        for i, group in enumerate(task_groups) if group
     )
-    parallel_commands += " & wait"  # Ensure all parallel commands complete
+
+    parallel_commands += " & wait"  # Ensure all parallel commands finish before the job exits
 
     job_index = i // tasks_per_job
     job_name = job_name_template.format(job_index=job_index)
